ee23e63f6561534fd38678e1e717aafdf5397eb7
Pontus Edskär <pe5107@ad.lystkom.se>
2021-04-12 10:50:39 +0200

    de fyra räknesätten

--- kalkylator.py
+++ kalkylator.py
@@ -12,3 +12,32 @@ def multiply(x, y):
 
 def devide(x, y):
     return x / y
+
+
+print("Välj Räknesätt")
+print("1.addera")
+print("2.subbtrahera")
+print("3.multiplicera")
+print("4.dividera")
+
+choice = input("Välj räknesätt(1/2/3/4): ")
+if choice in ('1' , '2' , '3' , '4'):
+    number1 = float(input("Det första nummret: "))
+    number2 = float(input("Det andra nummret: "))
+    x=number1
+    y=number2
+    
+    if choice == '1':  #testa
+        print (number1, "+", number2, "=", add(x, y))
+        
+    elif choice == '2':
+        print(number1, "-", number2, "=", subtract(x, y))
+        
+    elif choice == '3':
+        print(number1, "*", number2, "=", multiply(x, y))
+        
+    elif choice == '4':
+        print(number1, "/", number2, "=", devide(x, y))
+        
+        
+        
